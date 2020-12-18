37cf07b370ffabb026a5eecbb56179a8a9c3d0c6
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-18 21:46:02 +0700

    TVShowEntity

---
+++ app/src/main/java/com/arifikhsan/jetpackmoviecatalogue/entity/TVShowEntity.kt
@@ -0,0 +1,11 @@
+package com.arifikhsan.jetpackmoviecatalogue.entity
+
+class TVShowEntity (
+        val name: String,
+        val overview: String,
+        val popularity: Double,
+        val posterPath: String,
+        val firstAirDate: String,
+        val voteAverage: Double,
+        val voteCount: Int
+)
