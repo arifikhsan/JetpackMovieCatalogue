7807fdfbab228044a3ea8d4678c8a577a9e74238
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-29 15:10:16 +0700

    DetailTVShowViewModelTest

--- app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/ui/tv_shows/detail/DetailTVShowViewModelTest.kt
+++ app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/ui/tv_shows/detail/DetailTVShowViewModelTest.kt
@@ -1,52 +1,57 @@
 package com.arifikhsan.jetpackmoviecatalogue.ui.tv_shows.detail
 
+import androidx.lifecycle.MutableLiveData
 import com.arifikhsan.jetpackmoviecatalogue.data.repository.MovieRepository
+import com.arifikhsan.jetpackmoviecatalogue.data.response.GetTVShowDetailResponse
+import com.google.gson.Gson
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.InputStreamReader
 
-import org.junit.Assert.*
-
+@RunWith(MockitoJUnitRunner::class)
 class DetailTVShowViewModelTest {
 
-//    private lateinit var viewModel: DetailTVShowViewModel
-//    private val sampleTVShow = MovieRepository.getTVShows().first()
-//    private var tvShowId = sampleTVShow.id
-//    private val emptyTVShow = MovieRepository.getEmptyTVShow()
-//
-//    @Before
-//    fun setUp() {
-//        viewModel = DetailTVShowViewModel()
-//        viewModel.setSelectedTVShow(tvShowId)
-//    }
-//
-//    @Test
-//    fun getTVShow() {
-//        viewModel.setSelectedTVShow(sampleTVShow.id)
-//        val tvShow = viewModel.getTVShow()
-//
-//        assertEquals(sampleTVShow.id, tvShow.id)
-//        assertEquals(sampleTVShow.name, tvShow.name)
-//        assertEquals(sampleTVShow.overview, tvShow.overview)
-//        assertEquals(sampleTVShow.popularity, tvShow.popularity, 0.0001)
-//        assertEquals(sampleTVShow.posterPath, tvShow.posterPath)
-//        assertEquals(sampleTVShow.firstAirDate, tvShow.firstAirDate)
-//        assertEquals(sampleTVShow.voteAverage, tvShow.voteAverage, 0.0001)
-//        assertEquals(sampleTVShow.voteCount, tvShow.voteCount)
-//    }
-//
-//    @Test
-//    fun getEmptyTVShow() {
-//        viewModel.setSelectedTVShow(emptyTVShow.id)
-//        val tvShow = viewModel.getTVShow()
-//
-//        assertNotNull(tvShow)
-//        assertEquals(emptyTVShow.id, tvShow.id)
-//        assertEquals(emptyTVShow.name, tvShow.name)
-//        assertEquals(emptyTVShow.overview, tvShow.overview)
-//        assertEquals(emptyTVShow.popularity, tvShow.popularity, 0.0001)
-//        assertEquals(emptyTVShow.posterPath, tvShow.posterPath)
-//        assertEquals(emptyTVShow.firstAirDate, tvShow.firstAirDate)
-//        assertEquals(emptyTVShow.voteAverage, tvShow.voteAverage, 0.0001)
-//        assertEquals(emptyTVShow.voteCount, tvShow.voteCount)
-//    }
+    private lateinit var viewModel: DetailTVShowViewModel
+    private lateinit var sampleTVShow: GetTVShowDetailResponse
+    private var sampleTVShowId: Int = 0
+
+    @Mock
+    private lateinit var repository: MovieRepository
+
+    @Before
+    fun setUp() {
+        viewModel = DetailTVShowViewModel(repository)
+    }
+
+    @Test
+    fun getTVShow() {
+        sampleTVShow = Gson().fromJson(
+            InputStreamReader(javaClass.getResourceAsStream("get_tv_show.json")),
+            GetTVShowDetailResponse::class.java
+        )
+        sampleTVShow.id?.let { sampleTVShowId = it }
+        `when`(repository.getTVShowDetail(sampleTVShowId)).thenReturn(MutableLiveData(sampleTVShow))
+
+        viewModel.setTVShowId(sampleTVShowId)
+        viewModel.getTVShowDetail()
+        val tvShow = viewModel.tvShow.value
+
+        assertNotNull(tvShow)
+
+        assertEquals(sampleTVShow.id, tvShow?.id)
+        assertEquals(sampleTVShow.name, tvShow?.name)
+        assertEquals(sampleTVShow.overview, tvShow?.overview)
+        assertEquals(sampleTVShow.posterPath, tvShow?.posterPath)
+        assertEquals(sampleTVShow.firstAirDate, tvShow?.firstAirDate)
+        assertEquals(sampleTVShow.voteCount, tvShow?.voteCount)
+
+//        assertEquals(sampleTVShow.popularity, tvShow?.popularity, 0.0001)
+//        assertEquals(sampleTVShow.voteAverage, tvShow?.voteAverage, 0.0001)
+    }
 }
