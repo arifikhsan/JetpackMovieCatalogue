9cf86d782f27703fa5cb56892b1a461a5b0a440c
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-11 22:31:47 +0700

    aaaaaaaaa

--- app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/data/source/local/MovieLocalDataSourceTest.kt
+++ app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/data/source/local/MovieLocalDataSourceTest.kt
@@ -1,23 +1,61 @@
 package com.arifikhsan.jetpackmoviecatalogue.data.source.local
 
-//class MovieLocalDataSourceTest {
-//
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+//import androidx.test.platform.app.InstrumentationRegistry
+import com.arifikhsan.jetpackmoviecatalogue.data.source.local.entity.MovieEntity
+import com.arifikhsan.jetpackmoviecatalogue.injection.appModules
+import com.arifikhsan.jetpackmoviecatalogue.util.JsonHelper
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+import org.koin.test.inject
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+
+//@RunWith(MockitoJUnitRunner::class)
+class MovieLocalDataSourceTest : KoinTest {
+
 //    private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 //    private val database = AppDatabase.getInstance(context)
-//    private val local = MovieLocalDatasource(database)
-//    private val jsonHelper = JsonHelper()
-//
-//    private lateinit var movies: ArrayList<MovieEntity>
-//
-//    @Before
-//    fun setUp() {
-//        movies = MovieEntity.fromMoviesResponse(jsonHelper.getMovies()) as ArrayList<MovieEntity>
-//
-//        local.insertMovies(movies)
-//    }
-//
+
+//    private val local by inject<MovieLocalDatasource>()
+    private val local = mock(MovieLocalDatasource::class.java)
+    private val jsonHelper by inject<JsonHelper>()
+
+    private lateinit var movies: ArrayList<MovieEntity>
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+//        androidContext(context)
+        modules(appModules)
+    }
+
+    @Before
+    fun setUp() {
+        val localMovies = jsonHelper.getMovies()
+        movies = MovieEntity.fromMoviesResponse(localMovies) as ArrayList<MovieEntity>
+
+        local.insertMovies(movies)
+    }
+
 //    @Test
 //    fun getMovies() {
+//        var dummyMovies: DataSource.Factory<Int, MovieEntity>
+//        dummyMovies  = jsonHelper.getMovies()
+//        `when`(local.getMovies()).thenReturn(dummyMovies)
+//
 //        assertNotNull(local.getMovies())
 //    }
 //
@@ -26,4 +64,4 @@ package com.arifikhsan.jetpackmoviecatalogue.data.source.local
 //        val randomMovieId = movies.random().id
 //        assertNotNull(local.getMovie(randomMovieId))
 //    }
-//}
+}
--- app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/util/JsonHelperTest.kt
+++ app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/util/JsonHelperTest.kt
@@ -1,10 +1,12 @@
 package com.arifikhsan.jetpackmoviecatalogue.util
 
+import com.arifikhsan.jetpackmoviecatalogue.data.source.local.MovieLocalDatasource
 import com.arifikhsan.jetpackmoviecatalogue.injection.appModules
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
+import org.koin.android.ext.koin.androidContext
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
@@ -12,15 +14,19 @@ import org.koin.test.inject
 class JsonHelperTest : KoinTest {
 
     private val jsonHelper by inject<JsonHelper>()
+    private val local by inject<MovieLocalDatasource>()
+
 
     @get:Rule
     val koinTestRule = KoinTestRule.create {
+//        androidContext()
         modules(appModules)
     }
 
     @Test
     fun getMovies() {
         val movies = jsonHelper.getMovies()
+//        local.getMovies()
 
         assertNotNull(movies)
         assertEquals(20, movies.results?.size)
