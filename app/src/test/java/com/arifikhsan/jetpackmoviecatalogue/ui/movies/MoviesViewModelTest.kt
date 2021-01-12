b03bd449b874d05847b053fa30bfeecf239c9dba
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-12 14:49:11 +0700

    MoviesViewModelTest

--- app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/ui/movies/MoviesViewModelTest.kt
+++ app/src/test/java/com/arifikhsan/jetpackmoviecatalogue/ui/movies/MoviesViewModelTest.kt
@@ -1,9 +1,13 @@
 package com.arifikhsan.jetpackmoviecatalogue.ui.movies
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
 import com.arifikhsan.jetpackmoviecatalogue.data.repository.MovieRepository
-import com.arifikhsan.jetpackmoviecatalogue.data.source.remote.response.GetMoviesResponse
-import com.google.gson.Gson
+import com.arifikhsan.jetpackmoviecatalogue.data.source.local.entity.MovieEntity
+import com.arifikhsan.jetpackmoviecatalogue.valueobject.Resource
+import com.nhaarman.mockitokotlin2.verify
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import org.junit.Before
@@ -13,48 +17,45 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
-import java.io.InputStreamReader
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
-import com.nhaarman.mockitokotlin2.verify
 
-//@RunWith(MockitoJUnitRunner::class)
-//class MoviesViewModelTest {
-//
-//    private lateinit var viewModel: MoviesViewModel
-//
-//    @Mock
-//    private lateinit var repository: MovieRepository
-//
-//    @get:Rule
-//    var instantTaskExecutorRule = InstantTaskExecutorRule()
-//
-//    @Mock
-//    private lateinit var observer: Observer<GetMoviesResponse?>
-//
-//    @Before
-//    fun setUp() {
-//        viewModel = MoviesViewModel(repository)
-//    }
-//
-//    @Test
-//    fun getMovies() {
-//        val sampleMovies = Gson().fromJson(
-//            InputStreamReader(javaClass.getResourceAsStream("get_movies.json")),
-//            GetMoviesResponse::class.java
-//        )
-//        val movies = MutableLiveData<GetMoviesResponse>()
-//        movies.value = sampleMovies
-//
-//        `when`(repository.getMovies()).thenReturn(MutableLiveData(sampleMovies))
-//        viewModel.getMovies()
-//
-//        assertNotNull(sampleMovies)
-//        assertNotNull(viewModel.movies)
-//        assertEquals(sampleMovies, viewModel.movies.value)
-//        assertEquals(sampleMovies.results?.size, viewModel.movies.value?.results?.size)
-//
-//        viewModel.movies.observeForever(observer)
-//        verify(observer).onChanged(sampleMovies)
-//    }
-//}
+@RunWith(MockitoJUnitRunner::class)
+class MoviesViewModelTest {
+
+    private lateinit var viewModel: MoviesViewModel
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var repository: MovieRepository
+
+    @Mock
+    private lateinit var observer: Observer<Resource<PagedList<MovieEntity>>>
+
+    @Mock
+    private lateinit var pagedList: PagedList<MovieEntity>
+
+    @Before
+    fun setUp() {
+        viewModel = MoviesViewModel(repository)
+    }
+
+    @Test
+    fun getMovies() {
+        val dummyMovies = Resource.success(pagedList)
+        val randomNumber = (0 until 100).random()
+        `when`(dummyMovies.data?.size).thenReturn(randomNumber)
+        val movies = MutableLiveData<Resource<PagedList<MovieEntity>>>()
+        movies.value = dummyMovies
+
+        `when`(repository.getMovies()).thenReturn(movies)
+        val moviesEntity = viewModel.getMovies().value?.data
+
+        verify(repository).getMovies()
+        assertNotNull(moviesEntity)
+        assertEquals(randomNumber, moviesEntity?.size)
+
+        viewModel.getMovies().observeForever(observer)
+        verify(observer).onChanged(dummyMovies)
+    }
+}
